22eba5e2c16c782ae97be77deb5529d1c87921f7
ldrv565 <40745264+ldrv565@users.noreply.github.com>
2019-03-02 10:07:27 +0300

    digitalClock, menu

--- lab2/app/src/main/java/com/example/lab2/MainActivity.java
+++ lab2/app/src/main/java/com/example/lab2/MainActivity.java
@@ -8,37 +8,28 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.PopupMenu;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.DigitalClock;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView tvColor, tvSize;
+    DigitalClock clock;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tvColor = (TextView) findViewById(R.id.tvColor);
-        tvSize = (TextView) findViewById(R.id.tvSize);
-
-        // для tvColor и tvSize необходимо создавать контекстное меню
-        registerForContextMenu(tvColor);
-        registerForContextMenu(tvSize);
-
-        // button click - popupMenu
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(viewClickListener);
+        clock = (DigitalClock) findViewById(R.id.clock);
+        registerForContextMenu(clock);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
     {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.game_menu, menu);
+        inflater.inflate(R.menu.menu, menu);
         return true;
     }
 
@@ -46,42 +37,46 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item)
     {
         int id = item.getItemId();
-        TextView infoTextView = (TextView)
-                findViewById(R.id.textView);
         // Операции для выбранного пункта меню
         switch (id)
         {
-            case R.id.new_game:
-                infoTextView.setText("Выбран пункт Новая игра");
+            case R.id.color__red:
+                clock.setTextColor(Color.RED);
                 return true;
-            case R.id.help:
-                infoTextView.setText("Выбран пункт Справка");
+            case R.id.color__blue:
+                clock.setTextColor(Color.BLUE);
+                return true;
+            case R.id.color__green:
+                clock.setTextColor(Color.GREEN);
+                return true;
+            case R.id.size__small:
+                clock.setTextSize(8);
+            case R.id.size__medium:
+                clock.setTextSize(16);
+            case R.id.size__large:
+                clock.setTextSize(32);
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
 
-    final int MENU_COLOR_RED = 1;
-    final int MENU_COLOR_GREEN = 2;
-    final int MENU_COLOR_BLUE = 3;
-    final int MENU_SIZE_22 = 4;
-    final int MENU_SIZE_26 = 5;
-    final int MENU_SIZE_30 = 6;
+    final int ALPHA = 7;
+    final int ROTATE = 8;
+    final int SCALE = 9;
+    final int TRANSLATE = 10;
+    final int COMBO = 11;
 
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v,
                                     ContextMenu.ContextMenuInfo menuInfo) {
         switch (v.getId()) {
-            case R.id.tvColor:
-                menu.add(0, MENU_COLOR_RED, 0, "Red");
-                menu.add(0, MENU_COLOR_GREEN, 0, "Green");
-                menu.add(0, MENU_COLOR_BLUE, 0, "Blue");
-                break;
-            case R.id.tvSize:
-                menu.add(0, MENU_SIZE_22, 0, "22");
-                menu.add(0, MENU_SIZE_26, 0, "26");
-                menu.add(0, MENU_SIZE_30, 0, "30");
+            case R.id.clock:
+                menu.add(0, ALPHA, 0, "ALPHA");
+                menu.add(0, ROTATE, 0, "ROTATE");
+                menu.add(0, SCALE, 0, "SCALE");
+                menu.add(0, TRANSLATE, 0, "TRANSLATE");
+                menu.add(0, COMBO, 0, "COMBO");
                 break;
         }
     }
@@ -89,84 +84,28 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onContextItemSelected(MenuItem item) {
         // TODO Auto-generated method stub
+        Animation anim = null;
         switch (item.getItemId()) {
-            // пункты меню для tvColor
-            case MENU_COLOR_RED:
-                tvColor.setTextColor(Color.RED);
-                tvColor.setText("Text color = red");
+            // пункты меню для clock
+            case ALPHA:
+                anim = AnimationUtils.loadAnimation(this,R.anim.alpha);
                 break;
-            case MENU_COLOR_GREEN:
-                tvColor.setTextColor(Color.GREEN);
-                tvColor.setText("Text color = green");
+            case ROTATE:
+                anim = AnimationUtils.loadAnimation(this,R.anim.rotate);
                 break;
-            case MENU_COLOR_BLUE:
-                tvColor.setTextColor(Color.BLUE);
-                tvColor.setText("Text color = blue");
+            case SCALE:
+                anim = AnimationUtils.loadAnimation(this,R.anim.scale);
                 break;
-            // пункты меню для tvSize
-            case MENU_SIZE_22:
-                tvSize.setTextSize(22);
-                tvSize.setText("Text size = 22");
+            case TRANSLATE:
+                anim = AnimationUtils.loadAnimation(this,R.anim.translate);
                 break;
-            case MENU_SIZE_26:
-                tvSize.setTextSize(26);
-                tvSize.setText("Text size = 26");
+            case COMBO:
+                anim = AnimationUtils.loadAnimation(this,R.anim.combo);
                 break;
-            case MENU_SIZE_30:
-                tvSize.setTextSize(30);
-                tvSize.setText("Text size = 30");
-                break;
+        }
+        if (anim != null) {
+            clock.startAnimation(anim);
         }
         return super.onContextItemSelected(item);
-    }
-
-    View.OnClickListener viewClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            showPopupMenu(v);
-        }
-    };
-
-    private void showPopupMenu(View v) {
-        PopupMenu popupMenu = new PopupMenu(this, v);
-        popupMenu.inflate(R.menu.popupmenu); // Для Android 4.0
-        // для версии Android 3.0 нужно использовать длинный вариант
-        // popupMenu.getMenuInflater().inflate(R.menu.popupmenu,
-        // popupMenu.getMenu());
-        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                // Toast.makeText(PopupMenuDemoActivity.this,
-                // item.toString(), Toast.LENGTH_LONG).show();
-                // return true;
-                switch (item.getItemId()) {
-                    case R.id.menu1:
-                        Toast.makeText(getApplicationContext(),
-                                "Вы выбрали PopupMenu 1",
-                                Toast.LENGTH_SHORT).show();
-                        return true;
-                    case R.id.menu2:
-                        Toast.makeText(getApplicationContext(),
-                                "Вы выбрали PopupMenu 2",
-                                Toast.LENGTH_SHORT).show();
-                        return true;
-                    case R.id.menu3:
-                        Toast.makeText(getApplicationContext(),
-                                "Вы выбрали PopupMenu 3",
-                                Toast.LENGTH_SHORT).show();
-                        return true;
-                    default:
-                        return false;
-                }
-            }
-        });
-        popupMenu.setOnDismissListener(new PopupMenu.OnDismissListener() {
-            @Override
-            public void onDismiss(PopupMenu menu) {
-               Toast.makeText(getApplicationContext(), "onDismiss",
-                       Toast.LENGTH_SHORT).show();
-            }
-        });
-        popupMenu.show();
     }
 }
