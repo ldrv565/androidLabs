3f721c42e98d07c5710ce5688d8ec879e4a92597
ldrv565 <40745264+ldrv565@users.noreply.github.com>
2019-03-02 12:48:32 +0300

    Логи, уведомления.
    Готово.

--- lab2/app/src/main/java/com/example/lab2/MainActivity.java
+++ lab2/app/src/main/java/com/example/lab2/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.lab2;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.ContextMenu;
 import android.view.Gravity;
 import android.view.Menu;
@@ -13,6 +14,7 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.DigitalClock;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -46,31 +48,49 @@ public class MainActivity extends AppCompatActivity {
         {
             case R.id.color__red:
                 clock.setTextColor(Color.RED);
+                Log.d("SET_color","red");
+                Toast.makeText(this,"red", Toast.LENGTH_SHORT).show();
                 return true;
             case R.id.color__blue:
                 clock.setTextColor(Color.BLUE);
+                Log.d("SET_color","blue");
+                Toast.makeText(this,"blue", Toast.LENGTH_SHORT).show();
                 return true;
             case R.id.color__green:
                 clock.setTextColor(Color.GREEN);
+                Log.d("SET_color","green");
+                Toast.makeText(this,"green", Toast.LENGTH_SHORT).show();
                 return true;
             case R.id.size__small:
                 clock.setTextSize(8);
+                Log.d("SET_size","small");
+                Toast.makeText(this,"small", Toast.LENGTH_SHORT).show();
                 return true;
             case R.id.size__medium:
                 clock.setTextSize(16);
+                Log.d("SET_size","medium");
+                Toast.makeText(this,"medium", Toast.LENGTH_SHORT).show();
                 return true;
             case R.id.size__large:
                 clock.setTextSize(32);
+                Log.d("SET_size","large");
+                Toast.makeText(this,"large", Toast.LENGTH_SHORT).show();
                 return true;
             case R.id.position__top_left:
                 window.setGravity(Gravity.TOP);
+                Log.d("SET_position","top_left");
+                Toast.makeText(this,"top_left", Toast.LENGTH_SHORT).show();
                 return true;
             case R.id.position__center:
                 window.setGravity(Gravity.CENTER);
+                Log.d("SET_position","center");
+                Toast.makeText(this,"center", Toast.LENGTH_SHORT).show();
                 return true;
             case R.id.position__bottom_right:
                 window.setHorizontalGravity(Gravity.END);
                 window.setVerticalGravity(Gravity.BOTTOM);
+                Log.d("SET_position","bottom_right");
+                Toast.makeText(this,"bottom_right", Toast.LENGTH_SHORT).show();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
@@ -101,27 +121,35 @@ public class MainActivity extends AppCompatActivity {
     public boolean onContextItemSelected(MenuItem item) {
         // TODO Auto-generated method stub
         Animation anim = null;
+        String animationType = "null";
         switch (item.getItemId()) {
             // пункты меню для clock
             case ALPHA:
                 anim = AnimationUtils.loadAnimation(this,R.anim.alpha);
+                animationType = "alpha";
                 break;
             case ROTATE:
                 anim = AnimationUtils.loadAnimation(this,R.anim.rotate);
+                animationType = "rotate";
                 break;
             case SCALE:
                 anim = AnimationUtils.loadAnimation(this,R.anim.scale);
+                animationType = "scale";
                 break;
             case TRANSLATE:
                 anim = AnimationUtils.loadAnimation(this,R.anim.translate);
+                animationType = "translate";
                 break;
             case COMBO:
                 anim = AnimationUtils.loadAnimation(this,R.anim.combo);
+                animationType = "combo";
                 break;
         }
         if (anim != null) {
             clock.startAnimation(anim);
         }
+        Log.d("Animation_type", animationType);
+        Toast.makeText(this, animationType, Toast.LENGTH_SHORT).show();
         return super.onContextItemSelected(item);
     }
 }
